140a55d619279b28739db53e98c6150093ec4c73
Jesus Martinez <jesus.martinez37@yahoo.com>
2025-12-04 23:03:46 -0600

    Adding files for Hw

--- four-bit-alu/MUX/16_to_1_MUX.v
+++ four-bit-alu/MUX/16_to_1_MUX.v
@@ -1,1 +1,16 @@
-//placeholder
+//16x1 multiplexer 
+
+module sixteenToOneMux (
+    input [15:0]a,
+    input [3:0] s,
+    output f
+);
+    wire [3:0] y; 
+
+    fourToOneMux muxA(.a(a[0]),  .b(a[1]),  .c(a[2]),  .d(a[3]),  .s0(s[0]), .s1(s[1]), .f(y[0]));
+    fourToOneMux muxB(.a(a[4]),  .b(a[5]),  .c(a[6]),  .d(a[7]),  .s0(s[0]), .s1(s[1]), .f(y[1]));
+    fourToOneMux muxC(.a(a[8]),  .b(a[9]),  .c(a[10]), .d(a[11]), .s0(s[0]), .s1(s[1]), .f(y[2]));
+    fourToOneMux muxD(.a(a[12]), .b(a[13]), .c(a[14]), .d(a[15]), .s0(s[0]), .s1(s[1]), .f(y[3]));
+    fourToOneMux muxF(.a(y[0]),  .b(y[1]),  .c(y[2]),  .d(y[3]),  .s0(s[2]), .s1(s[3]), .f(f));
+
+endmodule
--- four-bit-alu/arithmetic/half_adder.v
+++ four-bit-alu/arithmetic/half_adder.v
@@ -1,1 +1,14 @@
 //place holder
+module fourBitFA(
+    input  [3:0] a,b,
+    input cin, 
+    output [3:0]s
+    output cout
+)
+assign s[0] = a[0] ^ b[0];
+assign s[1] = a[1] ^ b[1];
+assign s[2] = a[2] ^ b[2];
+assign s[3] = a[3] ^ b[3];
+
+
+endmodule 
